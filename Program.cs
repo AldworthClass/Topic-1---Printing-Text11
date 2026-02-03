0b3384cd0be8792f7f6da9570d36a5360b95f6ab
Steve Aldworth <s.aldworth@tvdsb.ca>
2026-02-03 13:05:37 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Topic_1___Printing_Text
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Aldworth
+            Console.WriteLine("Hello, World!");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("My name is Mr. Aldworth");
+            Console.WriteLine();
+            Console.Write("Computers ");
+            Console.WriteLine("Rule");
+            
+            Console.WriteLine("Press ENTER to continue");
+            Console.ReadLine();
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Clear();
+
+            Console.WriteLine("I like pizza!!!");
+            Console.WriteLine("Press ENTER to continue");
+            Console.ReadLine();
+            Console.ResetColor();
+
+           
+
+
+
+        }
+    }
+}
